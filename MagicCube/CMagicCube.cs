edb059a2c071beb9d81f2e62e193739377903a85
Константин Туркин <boxmen76@gmail.com>
2021-05-05 15:16:07 +0300

    Добавьте файлы проекта.

---
+++ MagicCube/CMagicCube.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCube
+{
+    public class CMagicCube
+    {
+        private int size;
+        private int[,] result;
+        private int[,] catalyst;
+
+        public int[,] GetMagicCube => result;
+
+        public CMagicCube(int size)
+        {
+            result = new int[size, size];
+            catalyst = new int[2 * size - 1, 2 * size - 1];
+            this.size = size;
+
+            Fill_numbers();
+            Rotation45();
+            Outskirts_replacement();
+            Center_replacement();
+        }
+
+        private void Outskirts_replacement()
+        {
+            int u = 2 * size - 1;
+            int v = (size - 1) / 2;
+            int g = 3 * (size - 1) / 2;
+
+            for (int i = 0; i < 2 * size - 1; i++)
+            {
+                for (int j = 0; j < 2 * size - 1; j++)
+                {
+                    if (catalyst[i, j] != 0 && 0 <= i && i < v)
+                    {
+                        catalyst[i + size, j] = catalyst[i, j];
+                    }
+                    else if (catalyst[i, j] != 0 && g < i && i <= u - 1)
+                    {
+                        catalyst[i - size, j] = catalyst[i, j];
+                    }
+                    else if (catalyst[i, j] != 0 && 0 <= j && j < v)
+                    {
+                        catalyst[i, j + size] = catalyst[i, j];
+                    }
+                    else if (catalyst[i, j] != 0 && g < j && j <= u - 1)
+                    {
+                        catalyst[i, j - size] = catalyst[i, j];
+                    }
+                }
+            }
+        }
+
+        private void Center_replacement()
+        {
+            int u = (size - 1) / 2;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = catalyst[u + i, u + j];
+                }
+            }
+        }
+
+        private void Rotation45()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    catalyst[size - 1 + i - j, i + j] = result[i, j];
+                }
+            }
+        }
+
+        private void Fill_numbers()
+        {
+            int count = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = count++;
+                }
+            }
+        }
+    }
+}
---
+++ TestLib/TestLibrary.cs
@@ -0,0 +1,71 @@
+﻿using MagicCube;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    class TestLibrary
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+
+            CMagicCube gen = new CMagicCube(size);
+
+            gen.GetMagicCube.Print();
+            Console.WriteLine("Это {0}магический квадрат.", gen.GetMagicCube.TestOnMagicCube(size) ? "" : "не ");
+
+            Console.ReadKey();
+        }
+    }
+
+    static class AddFuncion
+    {
+        public static bool TestOnMagicCube(this int[,] array2d, int size)
+        {
+            int control_num = size * (size * size + 1) / 2;
+
+            int[] inline = new int[size];
+            int[] incolu = new int[size];
+            int[] indiag = new int[2];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j)
+                    {
+                        indiag[0] += array2d[i, j];
+                    }
+                    if (i + j == size - 1)
+                    {
+                        indiag[1] += array2d[i, j];
+                    }
+                    inline[i] += array2d[i, j];
+                    incolu[j] += array2d[i, j];
+                }
+            }
+
+            return inline.All(u => u == control_num) && incolu.All(u => u == control_num) && indiag.All(u => u == control_num);
+        }
+
+        public static void Print(this int[,] array2d)
+        {
+            int i = 0;
+
+            foreach (var item in array2d)
+            {
+                Console.Write("{0,2} ", item);
+
+                if (i++ == array2d.GetLength(0) - 1)
+                {
+                    Console.WriteLine();
+                    i = 0;
+                }
+            }
+        }
+    }
+}
